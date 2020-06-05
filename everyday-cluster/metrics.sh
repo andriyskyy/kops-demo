32be0220cebc2a417b2cfbefa5bcd0891a8c80cf
Dmytro M <dmytro.minochkin@gmail.com>
2020-06-05 11:43:22 +0300

    metrics-server

---
+++ everyday-cluster/metrics.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Pretty metrics for dashboard
+# They will or will not work out of the box :) 
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
