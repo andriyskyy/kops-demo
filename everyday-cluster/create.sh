48c35da181c96ab073e9720bd72dd2546d55d573
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2021-02-10 14:11:49 +0200

    Update create.sh

--- everyday-cluster/create.sh
+++ everyday-cluster/create.sh
@@ -2,8 +2,8 @@
 kops create cluster \
     --name ${NAME} \
     --state ${KOPS_STATE_STORE} \
-    --node-count 3 \
-    --master-count=3 \
+    --node-count 2 \
+    --master-count=2 \
     --zones us-east-1a,us-east-1b,us-east-1c \
     --master-zones us-east-1a,us-east-1b,us-east-1c \
     --node-size t2.small \
