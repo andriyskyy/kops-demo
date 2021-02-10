937e551cec4be3a7625030b1e3ab360470273303
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2021-02-10 13:17:30 +0200

    Update init.sh

--- everyday-cluster/init.sh
+++ everyday-cluster/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # You should source this file
-export NAME="KOPS NAME OF YOUR CLUSTER" #SHOULD END WITH .k8s.local
-export KOPS_STATE_STORE="s3://INSERT S3 BUCKET HERE"
+export NAME="coolcluster84" #SHOULD END WITH .k8s.local
+export KOPS_STATE_STORE="s3://kops-bucket-example-1984"
